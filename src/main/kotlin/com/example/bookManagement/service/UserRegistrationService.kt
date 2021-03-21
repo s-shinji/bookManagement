aa9a1bfb74392676cc867dba0064cb78df8d7ab9
shinji <sshin8916@gmail.com>
2021-03-21 12:12:26 +0900

    新規ユーザ登録機能実装

---
+++ src/main/kotlin/com/example/bookManagement/service/UserRegistrationService.kt
@@ -0,0 +1,31 @@
+package com.example.bookManagement.service
+
+import com.example.bookManagement.entity.User
+import com.example.bookManagement.mapper.UserRegistrationMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional
+class UserRegistrationService {
+    @Autowired
+    lateinit var userRegistrationMapper: UserRegistrationMapper
+    @Autowired
+    lateinit var passwordEncoder: PasswordEncoder
+
+    fun registerUser(regUserInfo: User) {
+        regUserInfo.password = passwordEncoder.encode(regUserInfo.password)
+        userRegistrationMapper.registerUser(regUserInfo);
+    }
+    fun findByName(name: String): String? {
+        if(userRegistrationMapper.findByName(name) == null) return "OK"
+        else return "すでに登録済みのユーザー名です"
+    }
+    fun findByEmail(email: String): String? {
+        if(userRegistrationMapper.findByName(email) == null) return "OK"
+        else return "すでに登録済みのメールアドレスです"
+    }
+
+}
